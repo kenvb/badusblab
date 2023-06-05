7a5abd797ed55832adc9147721534148f80a8d9e
Ken Vanden Branden <kenvdb@gmail.com>
2023-06-05 11:08:43 +0200

    Update new-pastebin.ps1

--- new-pastebin.ps1
+++ new-pastebin.ps1
@@ -1,12 +1,12 @@
 
-#Demo 1, Something easy
+#Demo 1, Something easy. Userkey won't be used yet so everything is anonymous
 . .\logindata.ps1
 $Content    =   Invoke-WebRequest -Uri "https://ipinfo.io/json"
 $Title      =   "pastebin1"
 
 $Body = @{ 
     api_dev_key         = $DevKey;
-    api_user_key        = $UserKey;
+    #    api_user_key        = $UserKey;
     api_paste_name	    = $Title;
     api_paste_code      = $Content;
     api_paste_private   = "0"; # 0=public 1=unlisted 2=private
