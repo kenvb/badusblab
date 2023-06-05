d2fc1d8243a3d4a7acc38b8c1df877d0598633ce
Ken Vanden Branden <kenvdb@gmail.com>
2023-06-05 10:30:09 +0200

    Create Helloworld.ino

---
+++ Helloworld.ino
@@ -0,0 +1,9 @@
+#include “DigiKeyboard.h”
+void setup() {
+  DigiKeyboard.delay(5000);
+  // Will start printing wherever your keyboard cursor is located.
+  DigiKeyboard.print("Hello World");
+}
+void loop() {
+  // Nothing here
+}
